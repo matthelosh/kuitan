bf6a8a9e0753bb4fe5464d4e6bb4f1b68c2c0b00
Muhammad Soleh <matthelosh@gmail.com>
2025-04-12 06:23:10 +0700

    Update files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/templates/**/*.jinja"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
